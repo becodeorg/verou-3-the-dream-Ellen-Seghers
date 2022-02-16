6110523ce5d486a83a21437b92a2acee22b77550
Ellen Seghers <ellen.seghers1@gmail.com>
2022-02-16 11:40:19 +0100

    "Must haves done"

--- index.php
+++ index.php
@@ -1,12 +1,21 @@
 <?php
 
-var_dump($_GET);
-var_dump($_POST);
-echo $_POST['destination'];
+$dollar = $_POST['dollarName'];
+$euro = $dollar * 0.88;
 ?>
 
+<head>
+    <title>The Dream</title>
+</head>
+
+<h1>Calculator</h1>
+
 <form action="" method="post">
-    <label for="destination">Your destination</label>
-    <input type="text" name="destination" id="destination">
-    <input type="submit">
+    <label for="dollar">Your destination</label>
+    <input type="text" name="dollarName" id="dollar">
+    <input type="submit"><br>
+    <br>
+    <label for="euro">Output</label>
+    <input type="text" name="destination" id="euro" value="<?php echo $euro ?>">
 </form>
+
